f514088a7b3801ee7546c469f3179e6d9f71f483
Lazar Stojanović <lakisd98@gmail.com>
2022-07-24 20:34:10 +0200

    Add game functionality to play rounds and display winner

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Global counter which will be changed in playRound function and reset on refresh
 let counter = 0;
 
 // Generate random computer choice
@@ -16,24 +17,39 @@ function playRound(playerSelection, computerSelection) {
         counter--
         return "You Lose! Paper beats Rock";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        counter++
-        return "You Win! Rock beats Scissors"
+        counter++;
+        return "You Win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        counter++
-        return "You Win! Paper beats Rock"
+        counter++;
+        return "You Win! Paper beats Rock";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        counter--
-        return "You Lose! Scissors beats Paper"
+        counter--;
+        return "You Lose! Scissors beats Paper";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        counter--
-        return "You Lose! Rock beats Scissors"
+        counter--;
+        return "You Lose! Rock beats Scissors";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        counter++
-        return "You Win! Scissors beats Paper"
+        counter++;
+        return "You Win! Scissors beats Paper";
     } else {
-        return "It's a draw!"
+        return "It's a draw!";
     }
 }
 
 // Make a function called game where 5 rounds will be played
-function game() { }
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // Take user input every time
+        const userInput = prompt("Rock/Paper/Scissors");
+        console.log(playRound(userInput, getComputerChoice()));
+    }
+
+    // Check counter to see who is the winner in best of 5
+    if (counter < 0) {
+        return "Computer won!";
+    } else if (counter > 0) {
+        return "You won!";
+    } else {
+        return "It's a draw!";
+    }
+}
